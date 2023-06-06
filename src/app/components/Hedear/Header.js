4328c71525aa715d4d5e509e0dc1d8a2f8823b0b
DiegoEstela <die.estela@gmail.com>
2023-06-06 22:25:08 +0200

    test header

--- src/app/components/Hedear/Header.js
+++ src/app/components/Hedear/Header.js
@@ -1,18 +1,16 @@
 "use client";
 import styles from "./Header.module.css";
-import { useSearchParams, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { TbSmartHome, TbAdjustmentsFilled } from "react-icons/tb";
 
 export default function Header({ isOpen, setSidebarOpen }) {
-  const searchParams = useSearchParams();
-  const module = searchParams.get("module");
   const router = useRouter();
   return (
     <header className={styles.Header}>
       <div className={styles.icon} onClick={() => setSidebarOpen(!isOpen)}>
         <TbAdjustmentsFilled color="#f6f1f1" size="40px" />
       </div>
-      <h2 className={styles.Header_title}> {module ? module : ""}</h2>
+      <h2 className={styles.Header_title}> hola </h2>
       <div className={styles.icon} onClick={() => router.push("/")}>
         <TbSmartHome color="#f6f1f1" size="40px" />
       </div>
